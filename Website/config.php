918ee3cc5a45f3ed3225af02c2f8510687214e68
SteffVW <159015001+SteffVW@users.noreply.github.com>
2024-05-26 02:00:01 +0200

    Create config.php

---
+++ Website/config.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+$databaseName = 'visits';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $>
+?>
